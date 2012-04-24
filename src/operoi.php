040badd3e4806f9c99ac8ba9dd19f16e320cbc14
Sampo Laurila <sampo.laurila@gmail.com>
2012-04-24 12:39:34 +0300

    operoi.php tyyliviilaus

--- src/operoi.php
+++ src/operoi.php
@@ -7,47 +7,39 @@ $dbdata = "host=localhost dbname=ohtu user=ohtu password=ohtuproju";
 echo "<html><head><title>Operoi</title></head><body>";
 
 if ($_POST["tyyppi"]=="lisaa") {
-echo "lisays<br/>";
-$viite = new Viite();
-$tiedot = array("author"=>$_POST["author"], "year"=>$_POST["year"], "name"=>$_POST["name"],"key"=>$_POST["key"], "type"=>$_POST["type"]);
-$lisatiedot = array();
-foreach($_POST as $k => $v) {
-	if($_POST[$k]==""){
-	} else{
-		if($k=="author" || $k== "year" || $k== "name" || $k== "key" || $k== "type" ){
-		} else{
-			$lisatiedot[$k] = $v;
+    echo "lisays<br/>";
+    $viite = new Viite();
+    $tiedot = array("author"=>$_POST["author"], "year"=>$_POST["year"], "name"=>$_POST["name"],"key"=>$_POST["key"], "type"=>$_POST["type"]);
+    $lisatiedot = array();
+    foreach ($_POST as $k => $v) {
+        if ($_POST[$k]=="") {
+	} else {
+                if ($k=="author" || $k== "year" || $k== "name" || $k== "key" || $k== "type" ) {
+		} else {
+                    $lisatiedot[$k] = $v;
 		}
 	}
-	
-}
-$viite->lueDatat($tiedot, $lisatiedot, 0);
-//toteutetaan t�hän lisääysysashajöasjöglsmgknlössgmibgoköø
-insert($_POST);
-}
 
-else if ($_POST["tyyppi"]=="poista") {
+    }
+    $viite->lueDatat($tiedot, $lisatiedot, 0);
+    //toteutetaan t�hän lisääysysashajöasjöglsmgknlössgmibgoköø
+    insert($_POST);
+} else if ($_POST["tyyppi"]=="poista") {
     echo "poisto<br/>";
     remove($_POST["id"]);
-}
-
-else if ($_POST["tyyppi"]=="tulosta") {
+} else if ($_POST["tyyppi"]=="tulosta") {
     echo "tulosta<br/>";
     $temp = $_POST["filename"];
     $filename = "./files/"."".$temp;
     if (file_exists($filename)) {
         unlink($filename);
     }
-    $file = fopen($filename,'a');
-    fclose ($file);
-
+    $file = fopen($filename, 'a');
+    fclose($file);
 
     $dbdata="host=localhost dbname=ohtu user=ohtu password=ohtuproju";
     $conn = pg_connect($dbdata);
     $result = pg_query($conn, "SELECT * FROM viite");
-
-
-
 
     while ($row = pg_fetch_array($result)) {
         $result2 = pg_query($conn, "SELECT * from lisatieto WHERE owner = $row[0]");
@@ -55,7 +47,7 @@ else if ($_POST["tyyppi"]=="tulosta") {
 
         while ($row2 = pg_fetch_row($result2)) {
             $extradata[$row2[1]] = $row2[2];
-	}
+        }
         printtex($filename, $row, $extradata);
     }
 
