35f1731f020820b9c820e6cc94d736dfe0815a91
Farsun <ville.kukkola@helsinki.fi>
2012-04-24 12:27:44 +0300

    Update src/viite.php

--- src/viite.php
+++ src/viite.php
@@ -1,10 +1,16 @@
 <?php
+/**
+* Sisältää viitteen tiedot, sekä funktiot niiden käsittelyyn.
+*/
 class Viite
 {
     private $_tiedot = array();
     private $_lisatiedot = array();
     private $_tagit = array();
 
+    /**
+    * Lukee 3 taulukkoa viitteen tiedoiksi.
+    */
     public function lueDatat($tieto, $lisatieto, $tag)
     {
         //all parameters should be arrays!
@@ -13,16 +19,25 @@ class Viite
         $this->_tagit = $tag;
     }
 
+    /**
+    * Palauttaa viitteen perustiedot.
+    */
     public function getTiedot()
     {
         return $this->_tiedot;
     }
 
+    /**
+    * Palauttaa viitteen lisätiedot.
+    */
     public function getLisatiedot()
     {
         return $this->_lisatiedot;
     }
 
+    /**
+    * Palauttaa viitteen tagit.
+    */
     public function getTagit()
     {
         return $this->_tagit;
