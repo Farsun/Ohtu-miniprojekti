3d948a7e0f13e594de3989a86c6872e3afca8ee5
Farsun <ville.kukkola@helsinki.fi>
2012-04-24 10:50:28 +0300

    Update src/viite.php

--- src/viite.php
+++ src/viite.php
@@ -5,11 +5,6 @@ class Viite
 	private $lisatiedot = array();
 	private $tagit = array();
 
-	public function lueData(){
-	
-	}
-
-	//for testing purposes
 	public function lueDatat($tieto, $lisatieto, $tag){
 	//all parameters should be arrays!
 	$this->tiedot = $tieto;
@@ -29,13 +24,5 @@ class Viite
 	return $this->tagit;
 	}
 
-	public function printAll(){
-		$tiedot=$this->tiedot;
-		echo "Title: $tiedot[3] Year: $tiedot[2] \n Author: $tiedot[1] \n Type: $tiedot[4]";
-	}
-
-	public function printBibtex(){
-	
-	}
 }
 ?>
